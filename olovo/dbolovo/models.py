e386a28d90a0e3d99f66e3ceee93154948be5e44
Luboš Pechar <lubos.pechar@prirodou.cz>
2024-10-07 13:17:29 +0200

    blacked

--- olovo/dbolovo/models.py
+++ olovo/dbolovo/models.py
@@ -1,6 +1,7 @@
 from django.contrib.gis.db import models
 from dbolovo.fields import SpecialYearField
 
+
 class LocationType(models.Model):
     """
     Model reprezentující typ lokality, především mokřady jako např. rybník, tůň, stoka.
@@ -21,22 +22,39 @@ class LocationType(models.Model):
     def __str__(self):
         return self.location_type
 
+
 class Location(models.Model):
     """
     Model reprezentující konkrétní lokalitu s jejím názvem, rokem a typem.
     Nepovinně je možné zadat i souřadnice jako bod.
     """
 
-    year = SpecialYearField(verbose_name="Rok", help_text="Rok, kdy byla lokalita zaznamenána")
-    name = models.CharField(max_length=200, verbose_name="Název lokality", help_text="Zadejte název lokality")
-    location_type = models.ForeignKey(LocationType, on_delete=models.CASCADE, verbose_name="Typ lokality", help_text="Vyberte typ lokality")
-    point = models.PointField(null=True, blank=True, verbose_name="Bod", help_text="Zadejte souřadnice lokality", srid=4326)
+    year = SpecialYearField(
+        verbose_name="Rok", help_text="Rok, kdy byla lokalita zaznamenána"
+    )
+    name = models.CharField(
+        max_length=200,
+        verbose_name="Název lokality",
+        help_text="Zadejte název lokality",
+    )
+    location_type = models.ForeignKey(
+        LocationType,
+        on_delete=models.CASCADE,
+        verbose_name="Typ lokality",
+        help_text="Vyberte typ lokality",
+    )
+    point = models.PointField(
+        null=True,
+        blank=True,
+        verbose_name="Bod",
+        help_text="Zadejte souřadnice lokality",
+        srid=4326,
+    )
 
     class Meta:
         verbose_name = "Lokalita"
         verbose_name_plural = "Lokality"
-        ordering = ['name', 'year']
+        ordering = ["name", "year"]
 
     def __str__(self):
         return f"{self.name} ({self.year})"
-
