2ecda126a0115ec8d588ab0b51285be90f442410
Luboš Pechar <lubos.pechar@prirodou.cz>
2024-10-07 13:31:21 +0200

    upravy v modelu Sample

--- olovo/dbolovo/models.py
+++ olovo/dbolovo/models.py
@@ -23,33 +23,24 @@ class LocationType(models.Model):
         return self.location_type
 
 
+from django.contrib.gis.db import models
+from dbolovo.models import LocationType
+
 class Sample(models.Model):
     """
-    Model reprezentující vzorky odebraných sedimentů s názvem, rokem, typem a nepovinným bodem (souřadnice).
+    Model reprezentující vzorky odebraných sedimentů s rokem, názvem lokality, typem lokality a nepovinným bodem (souřadnice).
     """
 
-    year = SpecialYearField(verbose_name="Rok", help_text="Rok, kdy byl vzorek odebrán")
-    name = models.CharField(
-        max_length=200, verbose_name="Název vzorku", help_text="Zadejte název vzorku"
-    )
-    location_type = models.ForeignKey(
-        LocationType,
-        on_delete=models.CASCADE,
-        verbose_name="Typ lokality",
-        help_text="Vyberte typ lokality, ze které byl vzorek odebrán",
-    )
-    point = models.PointField(
-        null=True,
-        blank=True,
-        verbose_name="Bod",
-        help_text="Zadejte souřadnice odběru vzorku",
-        srid=4326,
-    )
+    year = models.IntegerField(verbose_name="Rok", help_text="Rok, kdy byl vzorek odebrán")
+    location_name = models.CharField(max_length=200, verbose_name="Název lokality", help_text="Zadejte název lokality, kde byl vzorek odebrán")
+    location_type = models.ForeignKey(LocationType, on_delete=models.CASCADE, verbose_name="Typ lokality", help_text="Vyberte typ lokality, ze které byl vzorek odebrán")
+    point = models.PointField(null=True, blank=True, verbose_name="Bod", help_text="Zadejte souřadnice odběru vzorku", srid=4326)
 
     class Meta:
         verbose_name = "Vzorek"
         verbose_name_plural = "Vzorky"
-        ordering = ["name", "year"]
+        ordering = ['location_name', 'year']
 
     def __str__(self):
-        return f"{self.name} ({self.year})"
+        return f"{self.location_name} ({self.year})"
+
