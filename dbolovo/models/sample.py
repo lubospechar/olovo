964970351f30701514cfcee374ba8a9b342790ac
Luboš Pechar <lubos.pechar@prirodou.cz>
2025-11-04 11:15:30 +0100

    Introduce `Sample` model

---
+++ dbolovo/models/sample.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+from dbolovo.fields import SpecialYearField
+from dbolovo.models.location import Location
+
+class Sample(models.Model):
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    year = SpecialYearField()
+    original_name = models.CharField(max_length=100)
+    sample_number = models.CharField(max_length=100, null=True, blank=True)
+    description = models.TextField()
+
+
