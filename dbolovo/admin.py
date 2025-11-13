b618531a3a8682399709116752874e02676bbe74
Luboš Pechar <lubos.pechar@prirodou.cz>
2025-11-13 11:24:35 +0100

    last_comit

--- dbolovo/admin.py
+++ dbolovo/admin.py
@@ -1,7 +1,7 @@
 from django.contrib.gis import admin
 from leaflet.admin import LeafletGeoAdmin
 
-from dbolovo.models import Parameter, LocationType, Location, Unit
+from dbolovo.models import Parameter, LocationType, Location, Unit, Sample
 
 
 @admin.register(Parameter)
@@ -54,4 +54,32 @@ class UnitAdmin(admin.ModelAdmin):
     ordering = ("name",)
     list_per_page = 50
 
+
+@admin.register(Sample)
+class SampleAdmin(admin.ModelAdmin):
+    # Sloupce v přehledu
+    list_display = ("original_name", "sample_number", "year", "location")
+
+    # Vyhledávání
+    search_fields = ("original_name", "sample_number", "description")
+
+    # Filtry v postranním panelu
+    list_filter = ("year",)
+
+    # Výchozí řazení a optimalizace dotazů
+    ordering = ("-year", "original_name")
+    list_select_related = ("location",)
+
+    # Ovládání FK pole (rychlejší při větším množství záznamů)
+    raw_id_fields = ("location",)
+
+    # Rozdělení do sekcí v detailu záznamu
+    fieldsets = (
+        (None, {"fields": ("location", "year")}),
+        ("Identifikace", {"fields": ("original_name", "sample_number")}),
+        ("Popis", {"fields": ("description",)}),
+    )
+
+    # Počet položek na stránku v changelistu
+    list_per_page = 50
 
